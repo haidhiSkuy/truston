c040a07a4abbf6a84e7165c7bba2f8eb2dd1e229
Haidhi Angkawijana <haidhiangkawijana@gmail.com>
2025-10-01 10:23:43 +0700

    example: latest infer

--- examples/infer_test.rs
+++ examples/infer_test.rs
@@ -1,21 +1,18 @@
 use std::vec;
-
-use tokio; 
-use truston::client::triton_client::TritonRestClient; 
 use ndarray::ArrayD;
 use ndarray::IxDyn;
-use truston::models::input_model::InferInput;
-
+use tokio;
+use truston::client::triton_client::TritonRestClient;
+use truston::client::io::InferInput;
 
 #[tokio::main]
-async fn main(){
-
-    let my_client = TritonRestClient::new("http://localhost:50000"); 
+async fn main() {
+    let my_client = TritonRestClient::new("http://localhost:50000");
     // let is_alive = my_client.is_server_live().await;
     // match is_alive {
     //     Ok(_) => println!("Server is live!"),
-    //     Err(e) => println!("error: {:#?}", e), 
-    // } 
+    //     Err(e) => println!("error: {:#?}", e),
+    // }
 
     let arr_ids: ArrayD<i64> = ArrayD::zeros(IxDyn(&[1, 128]));
     let input_ids = InferInput::from_ndarray("input_ids", arr_ids);
@@ -23,14 +20,11 @@ async fn main(){
     let arr_attention_mask: ArrayD<i64> = ArrayD::zeros(IxDyn(&[1, 128]));
     let input_attention_mask = InferInput::from_ndarray("attention_mask", arr_attention_mask);
 
-
     let inputs = vec![input_ids, input_attention_mask];
-    
+
     let res = my_client.infer(inputs, "hierarchical_clf").await;
-    match res { 
-        Ok(r) => println!("result: {:#?}", r), 
-        Err(e) => println!("error: {:#?}", e),
+    match res {
+        Ok(r) => println!("Success: {:#?}", r),
+        Err(_) => println!("Error"),
     }
-
-
 }
