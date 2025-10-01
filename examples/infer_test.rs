beea642ed610935ddc45fe0d935bdc499b17fc18
Haidhi Angkawijana <haidhiangkawijana@gmail.com>
2025-10-01 14:47:42 +0700

    examples: test async infer

--- examples/infer_test.rs
+++ examples/infer_test.rs
@@ -1,30 +1,38 @@
-use std::vec;
-use ndarray::ArrayD;
-use ndarray::IxDyn;
+use std::sync::Arc;
+use ndarray::{ArrayD, IxDyn};
 use tokio;
 use truston::client::triton_client::TritonRestClient;
 use truston::client::io::InferInput;
+use truston::utils::errors::TrustonError;
 
-#[tokio::main]
-async fn main() {
-    let my_client = TritonRestClient::new("http://localhost:50000");
-    // let is_alive = my_client.is_server_live().await;
-    // match is_alive {
-    //     Ok(_) => println!("Server is live!"),
-    //     Err(e) => println!("error: {:#?}", e),
-    // }
-
-    let arr_ids: ArrayD<i64> = ArrayD::zeros(IxDyn(&[1, 128]));
+async fn run_infer(client: Arc<TritonRestClient>) -> Result<(), TrustonError> {
+    let arr_ids: ArrayD<i64> = ArrayD::zeros(IxDyn(&[32, 128]));
     let input_ids = InferInput::from_ndarray("input_ids", arr_ids);
 
-    let arr_attention_mask: ArrayD<i64> = ArrayD::zeros(IxDyn(&[1, 128]));
+    let arr_attention_mask: ArrayD<i64> = ArrayD::zeros(IxDyn(&[32, 128]));
     let input_attention_mask = InferInput::from_ndarray("attention_mask", arr_attention_mask);
 
     let inputs = vec![input_ids, input_attention_mask];
 
-    let res = my_client.infer(inputs, "hierarchical_clf").await;
-    match res {
-        Ok(r) => println!("Success: {:#?}", r),
-        Err(_) => println!("Error"),
-    }
+    let _ = client.infer(inputs, "hierarchical_clf").await?;
+    Ok(())
+}
+
+
+#[tokio::main]
+async fn main() {
+    let my_client = Arc::new(TritonRestClient::new("http://localhost:50000"));
+
+    let tasks: Vec<_> = (0..2)
+        .map(|_| {
+            let client = my_client.clone();
+            tokio::spawn(async move {
+                if let Err(e) = run_infer(client).await {
+                    eprintln!("Error: {:?}", e);
+                }
+            })
+        })
+        .collect();
+
+    futures::future::join_all(tasks).await;
 }
