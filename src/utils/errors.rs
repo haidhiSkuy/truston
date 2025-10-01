48476f9e15707e71d54ca315cf5c4e4ccbc33a51
Haidhi Angkawijana <haidhiangkawijana@gmail.com>
2025-10-01 10:08:50 +0700

    err: infer error handling

--- src/utils/errors.rs
+++ src/utils/errors.rs
@@ -2,11 +2,18 @@
 pub enum TrustonError {
     Http(String),
     HttpErrorResponse(u16, String),
-    InferRequestError(String), 
+    InferRequestError(String),
+    InferParseError(String),
 }
 
 impl From<reqwest::Error> for TrustonError {
     fn from(err: reqwest::Error) -> Self {
         TrustonError::Http(err.to_string())
+    }
+}
+
+impl From<String> for TrustonError {
+    fn from(err: String) -> Self {
+        TrustonError::InferRequestError(err)
     }
 }
